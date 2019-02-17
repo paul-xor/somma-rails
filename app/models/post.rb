435a3350d7f17023af8dab2634380a0dbe4650db
Pavel Smolov <palexsmol@gmail.com>
2019-02-16 21:13:51 -0800

    update views and jumbo

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :campaign
-  validates :title, presence: true, length: {minimum: 3, maximum: 50}
+  validates :title, presence: true, length: {minimum: 3, maximum: 150}
   validates :body, presence: true, length: {minimum: 10, maximum: 5000}
   validates :campaign_id, presence: true
 end
