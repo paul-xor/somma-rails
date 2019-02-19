3a27381e6c0bca330ecde6556b8402697bc058a5
Pavel Smolov <palexsmol@gmail.com>
2019-02-18 16:30:16 -0800

    footer change, add icon

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ApplicationRecord
       user.image = auth.info.image 
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      user.save
+      #user.save
     end
   end
 
